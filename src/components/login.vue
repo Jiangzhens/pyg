489dfd9054c00d6a2e90d41cc45ea8b65462e242
Jiangzhens <543234569@qq.com>
2019-02-16 11:35:49 +0800

    渲染home

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,20 +1,54 @@
 <template>
   <div class="login-wrap">
-    <el-form class="login-form" label-position="top" label-width="80px">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
       <h2>用户登录</h2>
       <el-form-item label="用户名">
-        <el-input ></el-input>
+        <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input ></el-input>
+        <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button  class="login-btn" type="success">登录</el-button>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="success">登录</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    //登录请求
+    handleLogin() {
+      this.$http
+        .post("login", this.formdata)
+        .then(res => {
+          const {
+            data: {
+              data,
+              meta: { msg, status }
+            }
+          } = res;
+          if (status === 200) {
+            this.$router.push({
+                name:'home'
+            })
+          } else {
+            this.$message.error(msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/base.css'
+import axios from 'axios'
+axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
