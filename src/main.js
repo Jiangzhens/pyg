641904d31525712fa93160916764cbbe5b78150f
Jiangzhens <543234569@qq.com>
2019-02-17 18:42:50 +0800

    用户列表渲染

--- src/main.js
+++ src/main.js
@@ -6,14 +6,19 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/base.css'
+import moment from 'moment'
 import axios from 'axios'
-axios.defaults.baseURL='http://localhost:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 Vue.prototype.$http = axios
 
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+// 全局过滤器 - 处理日期格式
+Vue.filter('fmtdate', (v) => {
+  return moment(v).format('YYYY-MM-DD');
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
