0fdc28cc563ad4b087b4a00d6619b98cf7081b89
Jiangzhens <543234569@qq.com>
2019-02-18 11:50:04 +0800

    用户列表完成

--- src/components/users.vue
+++ src/components/users.vue
@@ -35,12 +35,18 @@
       </el-table-column>
       <el-table-column label="用户状态" width="140">
         <template slot-scope="scope">
-          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          <el-switch
+            @change="changeState(scope.row)"
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+          ></el-switch>
         </template>
       </el-table-column>
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
           <el-button
+            @click="showDiaEditUser(scope.row)"
             v-model="scope.row.mg_state"
             type="primary"
             icon="el-icon-edit"
@@ -48,8 +54,22 @@
             size="mini"
             plain
           ></el-button>
-          <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
-          <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+          <el-button
+            @click="showMsgBoxDele(scope.row)"
+            type="danger"
+            icon="el-icon-delete"
+            circle
+            size="mini"
+            plain
+          ></el-button>
+          <el-button
+            @click="showDiaSetRole(scope.row)"
+            type="success"
+            icon="el-icon-check"
+            circle
+            size="mini"
+            plain
+          ></el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -85,6 +105,51 @@
         <el-button type="primary" @click="addUser()">确 定</el-button>
       </div>
     </el-dialog>
+
+    <!-- 对话框- 编辑 -->
+    <el-dialog title="编辑用户" :visible.sync="dialogFormVisibleEdit">
+      <!-- 表单 -->
+      <el-form label-position="left" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">
+          <el-input disabled v-model="formdata.username"></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱">
+          <el-input v-model="formdata.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="formdata.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+        <el-button type="primary" @click="editUser()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 对话框 - 角色-->
+    <el-dialog title="分配角色" :visible.sync="dialogFormVisibleRole">
+      <el-form label-position="left" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">{{formdata.username}}</el-form-item>
+        <el-form-item label="角色">
+          {{selectVal}}
+          <el-select v-model="selectVal" placeholder="请选择角色">
+            <el-option disabled label="请选择" :value="-1"></el-option>
+            <el-option
+              v-for="(item,i) in roles"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleRole = false">取 消</el-button>
+        <el-button type="primary" @click="setRole()">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
@@ -100,12 +165,19 @@ export default {
       list: [],
       // 对话框
       dialogFormVisibleAdd: false,
+      dialogFormVisibleEdit: false,
+      dialogFormVisibleRole: false,
       formdata: {
         username: "",
         password: "",
         email: "",
         mobile: ""
-      }
+      },
+      // 下拉框用的数据
+      selectVal: -1,
+      currUserId: -1,
+      // 角色数组
+      roles: []
     };
   },
   // 获取首屏数据的方法调用
@@ -113,6 +185,83 @@ export default {
     this.getTableData();
   },
   methods: {
+    // 分配角色- 发送请求
+    async setRole() {
+      const res = await this.$http.put(`users/${this.currUserId}/role`, {
+        rid: this.selectVal
+      });
+      const {
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        // 关闭
+        this.dialogFormVisibleRole = false;
+      }
+    },
+    // 分配角色 - 打开对话框
+    async showDiaSetRole(user) {
+      this.formdata = user;
+      this.currUserId = user.id;
+      this.dialogFormVisibleRole = true;
+      // 获取角色名称
+      const res = await this.$http.get(`roles`);
+      this.roles = res.data.data;
+      const res2 = await this.$http.get(`users/${user.id}`);
+      console.log(res2);
+      // 给下拉框v-model绑定的selectVal赋值
+      this.selectVal = res2.data.data.rid;
+    },
+    // 修改状态
+    async changeState(user) {
+      const res = await this.$http.put(
+        `users/${user.id}/state/${user.mg_state}`
+      );
+    },
+    // 编辑 - 发送请求
+    async editUser() {
+      const res = await this.$http.put(
+        `users/${this.formdata.id}`,
+        this.formdata
+      );
+      const {
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        // 关闭
+        this.dialogFormVisibleEdit = false;
+        // 更新表格
+        this.getTableData();
+      }
+    },
+    // 编辑 - 显示对话框
+    async showDiaEditUser(user) {
+      this.dialogFormVisibleEdit = true;
+      this.dialogFormVisibleEdit = true;
+      const res = await this.$http.get(`users/${user.id}`);
+    },
+    // 删除 - 显示确认框
+    showMsgBoxDele(user) {
+      this.$confirm("是否把我删掉?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          // id->用户id数据->user.id
+          const res = await this.$http.delete(`users/${user.id}`);
+          const {
+            meta: { msg, status }
+          } = res.data;
+          if (status === 200) {
+            this.$message.success(msg);
+            this.pagenum = 1;
+            this.getTableData();
+          }
+        })
+        .catch(() => {
+          this.$message.info("取消删除");
+        });
+    },
     // 添加-确定按钮
     async addUser() {
       const res = await this.$http.post("users", this.formdata);
