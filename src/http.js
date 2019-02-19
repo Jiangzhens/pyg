4d882c2ea1aa143403ae07f53a4f1899bdefb4a9
Jiangzhens <543234569@qq.com>
2019-02-19 21:09:01 +0800

    统一处理请求头

--- src/http.js
+++ src/http.js
@@ -3,8 +3,17 @@ import axios from 'axios'
 const HttpServer = {}
 HttpServer.install = function (Vue) {
     axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
-    const AUTH_TOKEN = localStorage.getItem("token");
-    axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+    axios.interceptors.request.use(function (config) {
+        if (config.url !== 'login') {
+            const AUTH_TOKEN = localStorage.getItem("token");
+            config.headers["Authorization"] = AUTH_TOKEN;
+        }
+        return config;
+
+    }, function (error) {
+        return Promise.reject(error);
+    })
+
     Vue.prototype.$http = axios
 }
 
