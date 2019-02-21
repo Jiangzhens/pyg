a63af91b71dcafeb95a9b41b3d1bf83b6d548c71
Jiangzhens <543234569@qq.com>
2019-02-21 09:55:55 +0800

    权限列表完成

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -1,22 +1,32 @@
 <template>
   <el-card class="box">
     <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
+    <el-table height="350px" :data="list" style="width: 100%">
+      <el-table-column type="index" label="#" width="160"></el-table-column>
+      <el-table-column prop="authName" label="权限名称" width="200"></el-table-column>
+      <el-table-column prop="path" label="路径" width="200"></el-table-column>
+      <el-table-column label="层级" width="200"></el-table-column>
+      <template slot-scope="scope">
+        <span v-if="scope.row.level==='0'">一级</span>
+        <span v-if="scope.row.level==='1'">二级</span>
+        <span v-if="scope.row.level==='2'">三级</span>
+      </template>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-  data(){
-return {
-  list:[]
-}
+  data() {
+    return {
+      list: []
+    };
   },
   created() {
     this.getTableData();
   },
   methods: {
     async getTableData() {
- 
       const res = await this.$http.get("rights/list");
       console.log(res);
       const {
@@ -24,7 +34,7 @@ return {
         meta: { msg, status }
       } = res.data;
       if (status === 200) {
-        this.list = data
+        this.list = data;
       }
     }
   }
