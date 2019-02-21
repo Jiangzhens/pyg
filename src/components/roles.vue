629f5ee6b0acea508e59000cb1c16ba27cc26120
Jiangzhens <543234569@qq.com>
2019-02-21 20:47:42 +0800

    三级权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -3,6 +3,38 @@
     <cus-bread level1="权限管理" level2="角色列表"></cus-bread>
     <el-button class="btn" type="success">添加角色</el-button>
     <el-table height="350px" :data="roles" style="width: 100%">
+      <el-table-column type="expand" width="80">
+        <template slot-scope="scope">
+          <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+            
+            <el-col :span="4">
+              <el-tag closable>{{item1.authName}}</el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="(item2,i) in item1.children" :key="item2.id">
+                <el-col :span="4">
+                  <el-tag closable type="success">{{item2.authName}}</el-tag>
+                  <i class="el-icon-arrow-right"></i> 
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="info"
+                    v-for="(item3,i) in item2.children"
+                    :key="item3.id"
+                  >{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+          <el-row v-if='scope.row.children.length === 0'>
+            <el-col>
+              <span>未分配权限</span>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column type="index" label="#" width="180"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
       <el-table-column prop="roleDesc" label="角色描述" width="300"></el-table-column>
@@ -62,6 +94,12 @@ export default {
 }
 .box {
   height: 100%;
+}
+.level1 {
+  margin-top: 10px;
+}
+.level2 {
+  margin-bottom: 5px;
 }
 </style>
 
